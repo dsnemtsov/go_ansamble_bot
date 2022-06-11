dce16fa335087441b13c72b812f0467a959ad462
Dmitrii Nemtsov <dimas224@yandex.ru>
2022-06-11 10:28:05 +0300

    Yandex disc

--- internal/process/process.go
+++ internal/process/process.go
@@ -6,11 +6,12 @@ import (
 )
 
 func Do(b *telebot.Bot) {
-	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
-	})
-
 	b.Handle("/card_number", func(c tele.Context) error {
 		return c.Send("5228 6005 9599 3934")
 	})
+
+	b.Handle("/yandex_disc", func(c tele.Context) error {
+		return c.Send("https://disk.yandex.ru/d/2OuuGeT8TxJu_w")
+	})
+
 }
